c44cfe2517a8c012000a0dd3c44cb891a03d2015
wa-le <adeagboadewale05@gmail.com>
2022-05-28 15:09:04 +0200

    task4 added

--- Task 2 Tableau.sql
+++ Task 2 Tableau.sql
@@ -10,6 +10,9 @@ select * from t_dept_manager;
 select * from t_departments;
 
 ## B.I Task 1
+## Compare the number of male managers to the number of female managers from different departments for each year
+## starting from 1990
+# Real Answer below
 
 select year(d.from_date) as the_year, e.gender, count(e.emp_no) as amount_of_employee
 from t_employees e join t_dept_emp d
@@ -27,9 +30,7 @@ on dm.emp_no = e.emp_no
 group by the_year, e.gender, dm.dept_no
 having the_year >= 1990;
 
-
-
-## correct
+## not correct
 
 select year(dm.from_date) as the_year, dp.dept_name, e.gender, count(dm.emp_no) as amount_of_managers
 from t_dept_manager dm join t_employees e
@@ -38,4 +39,41 @@ join t_departments dp on dm.dept_no = dp.dept_no
 group by the_year, e.gender, dp.dept_name
 having the_year >= 1990;
 
+## ALL ABOVE IS WRONG FOR THE B.I TASK2
 
+
+## B.I Task 1
+## Compare the number of male managers to the number of female managers from different departments for each year
+## starting from 1990
+
+SELECT d.dept_name, ee.gender, dm.emp_no, dm.from_date, dm.to_date, e.calendar_year,
+	CASE
+		WHEN YEAR(dm.to_date) >= e.calendar_year AND YEAR(dm.from_date) <= e.calendar_year THEN 1
+		ELSE 0
+	END AS active
+FROM
+	(SELECT 
+		YEAR(hire_date) AS calendar_year
+	FROM
+		t_employees
+	GROUP BY calendar_year) e
+		CROSS JOIN t_dept_manager dm
+        JOIN t_departments d ON dm.dept_no = d.Dept_no
+        JOIN t_employees ee ON dm.emp_no = ee.emp_no
+ORDER BY dm.emp_no, calendar_year;
+        
+
+## just checking the subquery from above
+Select * FROM
+	(SELECT 
+		YEAR(hire_date) AS calendar_year
+	FROM
+		t_employees
+	GROUP BY calendar_year) e
+		CROSS JOIN t_dept_manager dm
+        JOIN t_departments d ON dm.dept_no = d.Dept_no
+        JOIN t_employees ee ON dm.emp_no = ee.emp_no
+ORDER BY dm.emp_no, calendar_year;
+
+
+		
---
+++ Task3.sql
@@ -0,0 +1,24 @@
+use employees_mod;
+
+# salary, gender, year, dept
+
+# My solution
+select e.gender, d.dept_name, s.salary, year(de.to_date) as calendar_year
+from
+t_employees e join t_salaries s on e.emp_no = s.emp_no
+join t_dept_emp de on s.emp_no = de.emp_no
+join t_departments d on de.dept_no = d.dept_no
+Group by d.dept_name, e.gender, s.salary
+having calendar_year <= 2002;
+
+
+# Correct solution
+select e.gender, d.dept_name, Round(AVG(s.salary), 2) as salary, year(s.from_date) as calendar_year
+from
+t_employees e join t_salaries s on e.emp_no = s.emp_no
+join t_dept_emp de on s.emp_no = de.emp_no
+join t_departments d on de.dept_no = d.dept_no
+Group by d.dept_no, e.gender, calendar_year
+having calendar_year <= 2002
+order by d.dept_no;
+
